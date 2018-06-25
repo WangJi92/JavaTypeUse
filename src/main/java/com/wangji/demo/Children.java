39581f54641c51cf8308f6de21c48e655ae7266d
wangji <wangji@hikvision.com>
2018-06-25 21:01:02 +0800

    言归正传，下面讲解ResolvableType。ResolvableType为所有的java类型提供了统一的数据结构以及API，换句话说，一个ResolvableType对象就对应着一种java类型。我们可以通过ResolvableType对象获取类型携带的信息

--- src/main/java/com/wangji/demo/Children.java
+++ src/main/java/com/wangji/demo/Children.java
@@ -1,6 +1,7 @@
 package com.wangji.demo;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.ResolvableType;
 
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
@@ -37,6 +38,24 @@ public class Children extends Parent<String> implements IParent<Long> {
                 }
             }
         }
+        /**
+         言归正传，下面讲解ResolvableType。ResolvableType为所有的java类型提供了统一的数据结构以及API，换句话说，一个ResolvableType对象就对应着一种java类型。我们可以通过ResolvableType对象获取类型携带的信息（举例如下）：
+         1.getSuperType()：获取直接父类型
+         2.getInterfaces()：获取接口类型
+         3.getGeneric(int...)：获取类型携带的泛型类型
+         4.resolve()：Type对象到Class对象的转换
+
+         另外，ResolvableType的构造方法全部为私有的，我们不能直接new，只能使用其提供的静态方法进行类型获取：
+         1.forField(Field)：获取指定字段的类型
+         2.forMethodParameter(Method, int)：获取指定方法的指定形参的类型
+         3.forMethodReturnType(Method)：获取指定方法的返回值的类型
+         4.forClass(Class)：直接封装指定的类型
+         */
+        ResolvableType superResolvableType = ResolvableType.forClass(Children.class).getSuperType();
+        log.info("supper:"+superResolvableType.resolveGenerics()[0]);
+
+        ResolvableType superInterfaceResolvableType = ResolvableType.forClass(Children.class).getInterfaces()[0];
+        log.info("supper:"+superInterfaceResolvableType.resolveGenerics()[0]);
 
     }
 }
